e34d81926dd97a98137500059e8ac86cd06850f3
Adriangnr <adriancaslagnr@gmail.com>
2018-05-25 21:40:33 -0300

    Agrego dataset y coneccion a la base de datos.

---
+++ ConectorDB/ConectorDb.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+using FrbaHotel.Properties;
+
+namespace FrbaHotel.ConectorDB
+{
+    public class ConectorDb
+    {
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection con = new SqlConnection(Settings.Default.FrbaHotelConnectionString);
+            con.Open();
+            return con;
+        }
+    }
+}
